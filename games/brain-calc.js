287d55cd877da76a2c82328c5e3f01d4b1e30abb
Tim <lastpatrol09@gmail.com>
2023-09-01 16:48:26 +0300

    added games to games folder

---
+++ games/brain-calc.js
@@ -0,0 +1,35 @@
+const objective = 'What is the result of the expression?';
+
+const getRandom = (max) => Math.floor(Math.random() * max);
+
+const generateQuestion = () => {
+  const maxNum = 100;
+  const operators = ['+', '-', '*'];
+
+  const operandA = getRandom(maxNum);
+  const operandB = getRandom(maxNum);
+  const operatorIndex = getRandom(operators.length);
+  const operator = operators[operatorIndex];
+  const question = `${operandA} ${operator} ${operandB}`;
+  let answerNum;
+  switch (operator) {
+    case '+':
+      answerNum = operandA + operandB;
+      break;
+    case '-':
+      answerNum = operandA - operandB;
+      break;
+    case '*':
+      answerNum = operandA * operandB;
+      break;
+    default:
+      break;
+  }
+
+  const answer = answerNum.toString();
+  return [question, answer];
+};
+
+const game = [objective, generateQuestion];
+
+export default game;

b30c5e08ba26f7b57eed152d15ddeea5e9ab7733
Tim <lastpatrol09@gmail.com>
2023-09-01 16:43:21 +0300

    moved cli.js to index.js, recreated cli.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import playGame from '../src/cli.js';
+import playGame from '../src/index.js';
 import game from '../src/brain-even.js';
 
 playGame(game);
--- src/cli.js
+++ src/cli.js
@@ -1,28 +1,9 @@
 import readlineSync from 'readline-sync';
 
-const playGame = ([objective, generateQuestion]) => {
-  const launches = 3;
-
+const getNameAndGreet = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
-  console.log(objective);
-
-  for (let i = 0; i < launches; i += 1) {
-    const [question, correctAnswer] = generateQuestion();
-    console.log(`Question: ${question}`);
-    const userAnswer = readlineSync.question('Your answer: ');
-    if (userAnswer === correctAnswer) {
-      console.log('Correct!');
-    } else {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      console.log(`Let's try again, ${name}!`);
-      return 0;
-    }
-  }
-
-  console.log(`Congratulations, ${name}!`);
-  return 0;
 };
 
-export default playGame;
+export default getNameAndGreet;
